2a494dd2a27b0f0d4d94af5fe1eb194d53e5b2be
hwang822 <hwang822@.gmail.com>
2022-04-03 16:03:30 -0500

    init RestFullServiceAPI

---
+++ RestFulServiceAPI/Models/WeatherInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace RestFulServiceAPI.Models
+{
+    [DataContract]
+    public class WeatherInfo
+    {
+        [DataMember(Name = "location")]
+        public string Location { get; set; }
+        [DataMember(Name = "degree")]
+        public float Degree { get; set; }
+        [DataMember(Name = "dateTime")]
+        public DateTime DateTime { get; set; }
+
+    }
+}
---
+++ WebService.REST/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="WebService.REST.Global_asax" Language="vb" %>
