662abc55f139ebe26b1c34e4a8ae08c5af20c5b2
Henry Wang <hwang822@gmail.com>
2024-02-08 09:25:05 -0600

    update

--- SpringMVCdemo/src/main/java/com/example/demo/service/UserService.java
+++ SpringMVCdemo/src/main/java/com/example/demo/service/UserService.java
@@ -1,10 +1,15 @@
 package com.example.demo.service;
+
 import com.example.demo.api.model.User;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
 @Service
 
 public class UserService {
@@ -14,11 +19,11 @@ public class UserService {
 
         userList = new ArrayList<>();
 
-        User user1 = new User( 1, "Ida",  32,  "Ida@mail.com");
-        User user2 = new User( 2, "Hans",  26,  "Hans@mail.com");
-        User user3 = new User( 3, "Lars",  45,  "Lars@mail.com");
-        User user4 = new User( 4, "Ben",  32,  "Ben@mail.com");
-        User user5 = new User( 5, "Eva",  59,  "Eva@mail.com");
+        User user1 = new User(1, "Ida", 32, "Ida@mail.com");
+        User user2 = new User(2, "Hans", 26, "Hans@mail.com");
+        User user3 = new User(3, "Lars", 45, "Lars@mail.com");
+        User user4 = new User(4, "Ben", 32, "Ben@mail.com");
+        User user5 = new User(5, "Eva", 59, "Eva@mail.com");
 
         userList.addAll(Arrays.asList(user1, user2, user3, user4, user5));
 
@@ -26,9 +31,9 @@ public class UserService {
 
     public Optional<User> getUser(Integer id) {
         Optional optinal = Optional.empty();
-        for(User user : userList) {
+        for (User user : userList) {
 
-            if(user.getId() == id){
+            if (user.getId() == id) {
                 optinal = Optional.of(user);
                 return optinal;
 
@@ -37,8 +42,13 @@ public class UserService {
         return null;
     }
 
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return userList;
     }
 
+    public List<User> getAllUsersFromDB(){
+        String sql = "SELECT * FROM testDB";
+        //List<User> users = JdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(User.class);
+        return userList;
+    }
 }
