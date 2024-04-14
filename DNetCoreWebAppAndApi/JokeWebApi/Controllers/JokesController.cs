67b4b98d2272fc25de51ac27c25a3362f3a860f4
Henry Wang <hwang822@gmail.com>
2024-04-14 16:15:32 -0500

    add api

---
+++ DNetCoreWebAppAndApi/JokeWebApi/Controllers/JokesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using JokeWebApi.Models;
+
+namespace JokeWebApi.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JokesController : ControllerBase
+    {
+        [HttpGet]
+
+        //public async Task<IActionResult> GetAllJokes()
+        public async Task<ActionResult<List<Joke>>> GetAllJokes()
+        {
+            var jokes = new List<Joke>{
+                new Joke
+                {
+                    Id = 1,
+                    JokeQuestion = "Who is John?",
+                    JokeAnswer = "He is a dog"
+                }
+            };
+            return Ok(jokes);
+        }
+    }
+}
