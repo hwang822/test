eb80668f65f5c756e747c3ea8ac493eb0b853d49
Henry Wang <henry.wang@tea.texas.gov>
2024-06-01 15:03:46 -0500

    remove bin, obj

---
+++ DNetCoreWebAppAndApi/JokeWebApi/Services/BrandServices.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Identity.Client;
+using JokeWebApi.Models;
+using Newtonsoft.Json;
+
+namespace JokeWebApi.Services
+{
+	public class BrandServices : IBrandServices
+	{
+
+		public BrandServices()
+		{
+
+		}
+
+		public IList<Brand> GetDataList()
+		{
+
+			return null;
+		}
+
+	}
+}
---
+++ DNetCoreWebAppAndApi/JokeWebApi/Services/IBrandServices.cs
@@ -0,0 +1,6 @@
+﻿namespace JokeWebApi.Services
+{
+	public interface IBrandServices
+	{
+	}
+}
