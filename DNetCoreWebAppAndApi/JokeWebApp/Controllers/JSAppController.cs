e2260559d5e74bdf38fd464870de952fe60833ec
Henry Wang <henry.wang@tea.texas.gov>
2024-06-21 16:28:07 -0500

    Add JSApp

---
+++ DNetCoreWebAppAndApi/JokeWebApp/Controllers/JSAppController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using JokeWebApp.Data;
+using JokeWebApp.Models;
+
+namespace JokeWebApp.Controllers
+{
+    public class JSAppController : Controller
+    {
+        // GET: Jokes
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+    }
+}
